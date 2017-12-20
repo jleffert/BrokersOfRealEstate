4241272e89f7012806762b9e8c8b0bcc41fe53b6
Joshua Leffert <jleffert9@gmail.com>
2017-12-19 23:49:31 -0600

    Add contact us page with form, layout links, and validate in controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,23 @@
 class SiteController < ApplicationController
   def index
   end
+
+  def contact_us
+    @contact_form = ContactForm.new
+  end
+
+  def send_contact_form
+      @contact_form = ContactForm.new(contact_form_params)
+
+      if @contact_form.valid?
+
+      else
+        render :contact_us
+      end
+  end
+
+  private
+  def contact_form_params
+    params.require(:contact_form).permit(:email, :message)
+  end
 end
