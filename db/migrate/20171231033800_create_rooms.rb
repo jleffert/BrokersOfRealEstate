6eedcf010df37dc8f9f2302b86197a77cf0103b2
Joshua Leffert <jleffert9@gmail.com>
2017-12-30 23:01:09 -0600

    Finish basic importing logic for residential houses

--- db/migrate/20171231033800_create_rooms.rb
+++ db/migrate/20171231033800_create_rooms.rb
@@ -3,7 +3,7 @@ class CreateRooms < ActiveRecord::Migration[5.1]
     create_table :rooms do |t|
       t.string :category, null: false
       t.integer :width, null: false
-      t.integer :height, null: false
+      t.integer :length, null: false
       t.references :house, foreign_key: true, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231033800) do
+ActiveRecord::Schema.define(version: 20171231041330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20171231033800) do
   create_table "rooms", force: :cascade do |t|
     t.string "category", null: false
     t.integer "width", null: false
-    t.integer "height", null: false
+    t.integer "length", null: false
     t.bigint "house_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
