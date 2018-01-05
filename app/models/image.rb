42301bf60871f5fa591ee5d22479108d4bec54a6
Joshua Leffert <jleffert9@gmail.com>
2018-01-04 23:54:50 -0600

    Implement a basic lot sync
    Needs to be refactored to use delayed jobs.  Otherwise takes hours to proccess ~600.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,14 @@
 class Image < ApplicationRecord
+  belongs_to :imageable, polymorphic: true
+  
+  after_destroy :delete_s3_image
+
+  def delete_s3_image
+    s3 = Aws::S3::Client.new(region:'us-east-1')
+    puts self.url
+    s3.delete_object({
+      bucket: 'brokersofrealestate',
+      key: self.url.gsub('https://brokersofrealestate.s3.amazonaws.com/', '')
+    })
+  end
 end
--- lib/BORE/sync_objects.rb
+++ lib/BORE/sync_objects.rb
@@ -1,9 +1,25 @@
 module BORE
-  class Sync_objects
+  class SyncObjects 
     def self.sync_media
       rets = RubyRETS::RETS.new(ENV["RETS_USER_ID"], ENV["RETS_PASSWORD"], ENV["RETS_USER_AGENT"], 'http://rets172lax.raprets.com:6103')
-      properties = rets.search('VirtualMedia', 'Pic','(MediaType=|Pic)', '/Midlands/MIDL/getobject.aspx')
-      
+      response = rets.login('/Midlands/MIDL/login.aspx')
+
+      Lot.all.limit(1).each do |lot|
+        if lot.picture_count and lot.picture_count > 0
+          lot.images.destroy_all
+          response = rets.download("Property", "Photo", "#{lot.listing_rid}:*", '/Midlands/MIDL/getobject.aspx')
+
+          s3 = Aws::S3::Resource.new(region:'us-east-1')
+          bucket = s3.bucket('brokersofrealestate')
+          response.parsed.each do |image|
+            File.rename(image[:data].path, "#{image[:data].path}.jpg")
+            object = bucket.object("images/lots/#{lot.id}/#{File.basename(image[:data])}.jpg")
+            object.upload_file("#{image[:data].path}.jpg", acl: 'public-read')
+            Image.create(imageable_type: 'Lot', imageable_id: lot.id, url: object.public_url)
+            FileUtils.rm("#{image[:data].path}.jpg")
+          end
+        end
+      end
     end
   end
 end
