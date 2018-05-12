afce45ea1c31499a1086fac6127a2f0bb1c8ac94
Joshua Leffert <jleffert9@gmail.com>
2018-05-12 00:15:17 -0500

    Add basic house index page
    Add house index page to display all houses for sale.  Displays address, price, and a thumbnail of an image of the house.

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,9 @@
 class House < ApplicationRecord
+  paginates_per 25
+  belongs_to :lot
   has_many :rooms
+
+  def images
+    lot.images
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :lots
   get 'contact_us' => 'site#contact_us'
   post 'contact_form' => 'site#send_contact_form'
   root 'site#index'
+  resources :houses
 end
