a08ed4c59c8886f60f45c1250ec28ac385e0f15f
Joshua Leffert <jleffert9@gmail.com>
2018-05-12 00:13:44 -0500

    Update schema from previous commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105060808) do
+ActiveRecord::Schema.define(version: 20180111044740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20180105060808) do
     t.bigint "lot_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_bedrooms", default: 0
+    t.integer "total_bathrooms", default: 0
     t.index ["lot_id"], name: "index_houses_on_lot_id"
   end
 
