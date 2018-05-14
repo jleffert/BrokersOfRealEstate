9fd8da65f9bce0fe3431852a89b585d4ff5c9172
Joshua Leffert <jleffert9@gmail.com>
2018-05-13 23:58:25 -0500

    Add in house show page and hashes for converting abbreviations

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,4 +1,10 @@
 class Lot < ApplicationRecord
   has_one :house
   has_many :images, as: :imageable, dependent: :destroy
+
+  LOT_FEATURES = {
+    "SECL": "Secluded",
+    "PRIV": "Private Roadway",
+    "LEVE": "Lot Description: Level"
+  }
 end
